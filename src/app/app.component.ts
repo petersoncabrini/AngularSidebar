fb87d6354c24bc391b839d31e266c83e462684da
Peterson <petersoncabrini@gmail.com>
2024-07-29 13:42:02 -0300

    Iniciar implementacao da sidebar com exibicao dos menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
